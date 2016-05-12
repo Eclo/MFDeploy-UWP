f5a9638bdf6d1d858ca83f45486940041e47f001
sjmneves <sergio.neves@eclo.pt>
2016-05-12 19:38:09 +0100

    Complete code to deploy files into device (work in progress)

--- MFDeploy/MFDeploy/Models/DeployFiles.cs
+++ MFDeploy/MFDeploy/Models/DeployFiles.cs
@@ -5,12 +5,19 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.FileProperties;
+using Windows.System.Threading;
 
 namespace MFDeploy.Models
 {
     [ImplementPropertyChanged]
     public class DeployFile
     {
+        public StorageFile DFile { get; set; }
+
+        public BasicProperties FileProperties { get; set; }
+
         public string FileName { get; set; }
 
         public string FilePath { get; set; }
@@ -23,14 +30,22 @@ namespace MFDeploy.Models
 
         public bool Selected { get; set; }
 
-        public DeployFile(string fileName, string filePath, ulong fileBaseAddress, ulong fileSize, string fileTimeStamp, bool selected = true)
+//        public DeployFile(string fileName, string filePath, ulong fileBaseAddress, ulong fileSize, string fileTimeStamp, bool selected = true)
+        public DeployFile(StorageFile dFile, bool selected = true)
         {
-            FileName = fileName;
-            FilePath = filePath;
-            FileBaseAddress = fileBaseAddress;
-            FileSize = fileSize;
-            FileTimeStamp = fileTimeStamp;
+            DFile = dFile;
             Selected = selected;
+
+            // 
+            LaunchThreadToGetFileProperties();
+        }
+
+        private async void LaunchThreadToGetFileProperties()
+        {
+            await ThreadPool.RunAsync(async (t) =>
+            {
+                FileProperties = await DFile.GetBasicPropertiesAsync();
+            });
         }
     }
 }
